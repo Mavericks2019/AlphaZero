e22a2795e42863fb48f0bb8154919877a9610d06
1015673037@qq.com <shijiannan2014>
2021-04-27 22:24:48 +0800

    chass Board end;move;has_winner;show

--- game/game.py
+++ game/game.py
@@ -7,6 +7,9 @@ class Board:
         self.height = height
         self.n = n
         self.states = {}
+        self.players = [1, 1]  # 1:black 0:white
+        self.current_player = 1
+        self.availables = set([i for i in range(width * height)])
 
     def show_board(self):
         return windows(self.width, self.height)
@@ -17,6 +20,7 @@ class Board:
         3 4 5
         6 7 8
         5 = (1, 2)
+        6 = (2, 0)
         """
         h = move // self.width
         w = move % self.width
@@ -31,3 +35,52 @@ class Board:
         if move not in range(self.width * self.height):
             return -1
         return move
+
+    def has_a_winner(self):
+        width = self.width
+        height = self.height
+        states = self.states
+        n = self.n
+
+        moved = list(set(range(width * height)) - set(self.availables))
+        if len(moved) < self.n * 2 - 1:
+            return False, -1
+
+        for m in moved:
+            h = m // width
+            w = m % width
+            player = states[m]
+
+            # rows
+            if (w in range(width - n + 1) and
+                    len(set(states.get(i, -1) for i in range(m, m + n))) == 1):
+                return True, player
+            # cols
+            if (h in range(height - n + 1) and
+                    len(set(states.get(i, -1) for i in range(m, m + n * width, width))) == 1):
+                return True, player
+            # \
+            if (w in range(width - n + 1) and h in range(height - n + 1) and
+                    len(set(states.get(i, -1) for i in range(m, m + n * (width + 1), width + 1))) == 1):
+                return True, player
+            # /
+            if (w in range(n - 1, width) and h in range(height - n + 1) and
+                    len(set(states.get(i, -1) for i in range(m, m + n * (width - 1), width - 1))) == 1):
+                return True, player
+
+        return False, -1
+
+    def game_end(self):
+        """Check whether the game is ended or not"""
+        win, winner = self.has_a_winner()
+        if win:
+            return True, winner
+        elif not len(self.availables):
+            return True, -1
+        return False, -1
+
+    def move(self, move):
+        self.states[move] = self.current_player
+        self.availables.remove(move)
+        self.current_player = -self.current_player
+
